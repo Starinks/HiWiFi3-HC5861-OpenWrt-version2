2e78c0f9fa8bf2bc2facfa183ff658bc15901f4e
Mahiru <41849505+Starinks@users.noreply.github.com>
2021-10-19 13:52:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,35 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#固件架构
+cat >> .config <<EOF
+CONFIG_TARGET_ramips=y
+CONFIG_TARGET_ramips_mt7620=y
+CONFIG_TARGET_ramips_mt7620_DEVICE_hiwifi_hc5861=y
+EOF
+
+#取消默认启用的软件
+cat >> .config <<EOF
+# CONFIG_PACKAGE_luci-app-accesscontrol is not set
+# CONFIG_PACKAGE_luci-app-vlmcsd is not set
+# CONFIG_PACKAGE_luci-app-unblockmusic is not set
+EOF
+
+#使固件支持tf卡扩展
+#cat >> .config <<EOF
+#CONFIG_PACKAGE_kmod-nls-cp437=y
+#CONFIG_PACKAGE_kmod-nls-iso8859-1=y
+#CONFIG_PACKAGE_kmod-nls-utf8=y
+#CONFIG_PACKAGE_kmod-sdhci=y
+#CONFIG_PACKAGE_kmod-sit=y
+#CONFIG_PACKAGE_block-mount=y
+#CONFIG_PACKAGE_kmod-fs-ext4=y
+#CONFIG_PACKAGE_kmod-fs-vfat=y
+#EOF
+
+#启用软件
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-app-argon-config=y
+CONFIG_PACKAGE_luci-app-jd-dailybonus=y
+EOF
